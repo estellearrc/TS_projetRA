7ad70f71c3527725d477e3458f48286a2fb242a6
estellearrc <estellearrc@github.com>
2018-11-19 00:24:42 +0100

    Début 3D

---
+++ scene3d.m
@@ -0,0 +1,14 @@
+function modele = scene3d()
+
+end
+function segment = construitSegment(pointDepart, pointArrivee, longueur)
+    [xd yd] = pointDepart;
+    [xa ya] = pointArrivee;
+    for i = yd:ya
+        for j = xd:xa
+            
+        end
+    end
+end
+function carre = construitCarre(segment, position)
+end
